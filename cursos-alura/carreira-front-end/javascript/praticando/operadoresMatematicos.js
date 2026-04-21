602d95e4ef639c2b9a0a18511a7005bbc0136b27
Kauan Santanna <1kn.santanna@gmail.com>
2026-04-21 17:42:01 -0300

    Aprendendo sobre operadores matematicos

---
+++ cursos-alura/carreira-front-end/javascript/praticando/operadoresMatematicos.js
@@ -0,0 +1,41 @@
+const numeroUm = 4;
+const numeroDois = 8;
+
+const subtracao = numeroUm - numeroDois;
+console.log("Substração:", subtracao);
+
+const adicao = numeroUm + numeroDois;
+console.log("Soma:", adicao);
+
+const divisao = numeroUm / numeroDois;
+console.log("Divisão:", divisao);
+
+const multiplicacao = numeroUm * numeroDois;
+console.log("Multiplicação:", multiplicacao);
+
+const contaComplexa = (numeroUm + numeroDois) * numeroUm;
+console.log("Conta complexa:", contaComplexa);
+
+const restoDivisao = 13 % 2;
+console.log("Resto da divisão:", restoDivisao);
+
+const potencia = 2 ** 3;
+console.log("Potência:", potencia);
+
+let contador = 5
+// contador = contador + 1
+contador++
+console.log("Incremento:", contador)
+
+// contador = contador - 1
+contador--
+console.log("Decremento:", contador)
+
+let numeroTres = 2
+
+numeroTres += 2
+// numeroTres -= 2
+// numeroTres /= 2
+// numeroTres *= 2
+
+console.log(numeroTres)
