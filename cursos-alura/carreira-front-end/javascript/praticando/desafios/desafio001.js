1859ad19fcef5534429a9d4b3ede51843d00bfd3
Kauan Santanna <1kn.santanna@gmail.com>
2026-05-04 21:16:42 -0300

    Primeiro desafio em JavaScript

---
+++ cursos-alura/carreira-front-end/javascript/praticando/desafios/desafio001.js
@@ -0,0 +1,11 @@
+console.log("Boas vindas a calculadora de troco!");
+
+let valorCompra = 17.5;
+let valorPago = 20;
+
+console.log("Valor da compra: R$", valorCompra);
+console.log("Valor recebido: R$", valorPago);
+
+let troco = valorPago - valorCompra;
+
+console.log("O valor do troco é: R$", troco);
