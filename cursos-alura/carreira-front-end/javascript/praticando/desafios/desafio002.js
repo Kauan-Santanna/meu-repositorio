9403a5538290bb83be5d05ff85f413fcfa7f66cd
Kauan Santanna <1kn.santanna@gmail.com>
2026-05-04 21:32:57 -0300

    Segundo desafio em JavaScript

---
+++ cursos-alura/carreira-front-end/javascript/praticando/desafios/desafio002.js
@@ -0,0 +1,8 @@
+console.log("=-".repeat(13));
+console.log("Verificação de maioridade");
+console.log("=-".repeat(13));
+
+idade = 21;
+maiorDeIdade = idade >= 18;
+
+console.log("A idade é maior ou igual a 18?", maiorDeIdade);
