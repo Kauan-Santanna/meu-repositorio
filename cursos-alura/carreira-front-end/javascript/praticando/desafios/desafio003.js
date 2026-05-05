b1010540cc1dcb1b95ba962269360414f0f2fb0b
Kauan Santanna <1kn.santanna@gmail.com>
2026-05-04 23:21:03 -0300

    Terceiro desafio em JS

---
+++ cursos-alura/carreira-front-end/javascript/praticando/desafios/desafio003.js
@@ -0,0 +1,11 @@
+console.log("=-".repeat(7));
+console.log("Média do Aluno");
+console.log("=-".repeat(12));
+
+let primeiraNota = 7;
+let segundaNota = 8;
+
+let mediaDoAluno = (primeiraNota + segundaNota) / 2;
+
+console.log("A média do aluno é:", mediaDoAluno);
+console.log("=-".repeat(12));
