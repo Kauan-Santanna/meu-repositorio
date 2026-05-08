8b0d4e7a8059e6682788285e47dbe53345c78d75
Kauan Santanna <1kn.santanna@gmail.com>
2026-05-07 23:21:33 -0300

    Quarto desafio em JavaScript

---
+++ cursos-alura/carreira-front-end/javascript/praticando/desafios/desafio004.js
@@ -0,0 +1,11 @@
+console.log("=-".repeat(9));
+console.log("Validação de senha");
+console.log("=-".repeat(9));
+
+let senha1 = "123456";
+let senha2 = "123456";
+
+let senhasIguais = senha1 === senha2;
+
+console.log("As senhas são iguais?", senhasIguais);
+console.log("=-".repeat(13));
