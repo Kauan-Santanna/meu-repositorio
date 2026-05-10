26c79dcd55d00a31554c2260157910eb57d39094
Kauan Santanna <1kn.santanna@gmail.com>
2026-05-10 12:36:01 -0300

    Desafio006 concluido em JavaScript

---
+++ cursos-alura/carreira-front-end/javascript/praticando/desafios/desafio006.js
@@ -0,0 +1,9 @@
+console.log("=-".repeat(10))
+console.log("Verificação de login")
+console.log("=-".repeat(10))
+
+let temLogin = true
+let temSenha = true
+
+console.log(temLogin && temSenha)
+console.log("=-".repeat(2))
