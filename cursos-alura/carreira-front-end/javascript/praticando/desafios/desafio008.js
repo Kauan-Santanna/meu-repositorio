9cef3922d76301706c3ed78e789b33eff9723c23
Kauan Santanna <1kn.santanna@gmail.com>
2026-05-10 21:16:49 -0300

    Desafio009 concluido em JavaScript

--- cursos-alura/carreira-front-end/javascript/praticando/desafios/desafio008.js
+++ cursos-alura/carreira-front-end/javascript/praticando/desafios/desafio008.js
@@ -1,10 +1,10 @@
-console.log("=-".repeat(10))
-console.log("Condições compostas")
-console.log("=-".repeat(10))
+console.log("=-".repeat(10));
+console.log("Condições compostas");
+console.log("=-".repeat(10));
 
-let numeroUm = 4
-let numeroDois = 4
+let numeroUm = 4;
+let numeroDois = 4;
 
-console.log("Os dois números são pares?", numeroUm % 2 === 0 && numeroDois % 2 === 0)
-console.log("Os dois números são iguais", numeroUm === numeroDois)
-console.log("=-".repeat(16))
+console.log("Os dois números são pares?", numeroUm % 2 === 0 && numeroDois % 2 === 0);
+console.log("Os dois números são iguais", numeroUm === numeroDois);
+console.log("=-".repeat(16));
---
+++ cursos-alura/carreira-front-end/javascript/praticando/desafios/desafio009.js
@@ -0,0 +1,11 @@
+console.log("=-".repeat(13));
+console.log("Calculadora de porcentagem");
+console.log("=-".repeat(13));
+
+let numero = 120;
+let percentual = 0.15;
+
+let resultado = numero * percentual;
+
+console.log("15% de 120 é igual a:", resultado);
+console.log("=-".repeat(13));
