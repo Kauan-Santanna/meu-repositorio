bd343faa82ee046f16e0ce7e18d2c9895a751218
Kauan Santanna <1kn.santanna@gmail.com>
2026-05-10 21:20:38 -0300

    Desafio010 concluido em JavaScript

---
+++ cursos-alura/carreira-front-end/javascript/praticando/desafios/desafio010.js
@@ -0,0 +1,7 @@
+console.log("=-".repeat(9));
+console.log("Ordem de operações");
+console.log("=-".repeat(9));
+
+// A multiplicação foi feita antes da soma pela ordem de operações da matematica, ou seja, primeiro vem multiplicação/divisão e depois adição/subtração
+console.log(2 + 3 * 5);
+console.log("=-".repeat(1));
