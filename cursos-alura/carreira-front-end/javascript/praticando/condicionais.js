5c02d885fb1a0b6662659d65cb043528b89414a7
Kauan Santanna <1kn.santanna@gmail.com>
2026-05-12 21:12:32 -0300

    Comecando a aprender sobre condicionais

---
+++ cursos-alura/carreira-front-end/javascript/praticando/condicionais.js
@@ -0,0 +1,28 @@
+// const idade = 19;
+
+// if (idade >= 18) {
+//   console.log("É maior de idade!");
+// } else {
+//   console.log("É menor de idade!");
+// }
+
+// if (idade < 18) {
+//   console.log("É menor de idade");
+// }
+
+// entre 9 e 10: excelente
+// entre 7 e 8: bom
+// entre 4 e 6: médio
+// entre 0 e 3: ruim
+
+const notaDoAluno = 6;
+
+if (notaDoAluno >= 9) {
+  console.log("Nota excelente!");
+} else if (notaDoAluno >= 7) {
+  console.log("Nota boa!");
+} else if (notaDoAluno >= 4) {
+  console.log("Nota mediana!");
+} else {
+  console.log("Nota ruim!");
+}
