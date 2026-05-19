34cbb6abe79198db8b6635bdd4cb96d323e231ca
Kauan-Santanna <1kn.santanna@gmail.com>
2026-05-19 19:49:03 -0300

    Desafio022 concluido com JavaScript

---
+++ cursos-alura/carreira-front-end/javascript/praticando/desafios/desafio022.js
@@ -0,0 +1,13 @@
+// Some todos os números de 1 a 100. Mostre o resultado no final com uma mensagem explicativa. 
+
+console.log("=-".repeat(8))
+console.log("Soma de 1 a 100")
+console.log("=-".repeat(8))
+
+let soma = 0
+
+for (let numero = 1; numero <= 100; numero++) {
+    soma = soma + numero
+}
+
+console.log(`Somando os números de 1 a 100, o resultado vai ser: ${soma}`)
