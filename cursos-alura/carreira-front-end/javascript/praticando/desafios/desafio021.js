b7a42fcc4fe74c33c3ba113efaf93c253fa4ee30
Kauan-Santanna <1kn.santanna@gmail.com>
2026-05-19 19:38:33 -0300

    Desafio021 concluido com JavaScript

---
+++ cursos-alura/carreira-front-end/javascript/praticando/desafios/desafio021.js
@@ -0,0 +1,11 @@
+// Use um for para mostrar no console os números de 1 até 10, um por linha.
+
+console.log("=-".repeat(9));
+console.log("Contador de 1 a 10");
+console.log("=-".repeat(9));
+
+for (let numero = 1; numero <= 10; numero++) {
+  console.log("Número:", numero);
+}
+
+console.log("=-".repeat(9));
