c764f25bf398b4b7c0534f31714bbb72ad31ca30
Kauan-Santanna <1kn.santanna@gmail.com>
2026-05-19 21:06:23 -0300

    Desafio024 concluido com JavaScript

---
+++ cursos-alura/carreira-front-end/javascript/praticando/desafios/desafio024.js
@@ -0,0 +1,15 @@
+// Use while para contar de 10 até 0 no console. Mostre uma mensagem no fim: "Contagem finalizada!".
+
+console.log("=-".repeat(10));
+console.log("Contagem regressiva");
+console.log("=-".repeat(10));
+
+let numero = 10;
+
+while (numero >= 0) {
+  console.log(numero);
+  numero--;
+}
+console.log("=-".repeat(10));
+console.log("Contagem finalizada");
+console.log("=-".repeat(10));
