2a749d3851d90cf7faba505abc111ebd953262ee
Kauan-Santanna <1kn.santanna@gmail.com>
2026-05-20 23:48:37 -0300

    Aprendendo mais sobre funcoes, Arrow function x function

--- cursos-alura/carreira-front-end/javascript/praticando/funcoes.js
+++ cursos-alura/carreira-front-end/javascript/praticando/funcoes.js
@@ -1,6 +1,12 @@
-function saudacao(nome) { // nome é um parametro de função saudacao
-    console.log("Olá,", nome)
-}
+// function saudacao(nome) { // nome é um parametro de função saudacao
+//     console.log("Olá,", nome)
+// }
+
+// const saudacao = (nome) => {
+//     console.log("Vida longa e próspera,", nome)
+// } 
+
+const saudacao = nome => console.log("Olá,", nome)
 
 saudacao("Kauan") // Kauan é o "argumento"
 saudacao("Aninha")
@@ -12,9 +18,15 @@ saudacao("Aninha")
 
 // calcularDobro(4)
 
-function calcularDobro(numero) {
-    return numero * 2
-}
+// function calcularDobro(numero) {
+//     return numero * 2
+// }
+
+// const calcularDobro = (numero) => {
+//     return numero * 2
+// }
+
+const calcularDobro = numero => numero * 2
 
 const numeroDobrado = calcularDobro(4)
 console.log("O dobro de 4 é:", numeroDobrado)
