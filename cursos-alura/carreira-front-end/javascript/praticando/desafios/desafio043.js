f8bde616477e27cad6f14b719760a28004e19d12
Kauan-Santanna <1kn.santanna@gmail.com>
2026-05-24 12:56:59 -0300

    Desafio043 concluido com JavaScript

---
+++ cursos-alura/carreira-front-end/javascript/praticando/desafios/desafio043.js
@@ -0,0 +1,10 @@
+// Crie um array com nomes de cidades e exiba no console a quantidade total de itens usando .length. 
+
+console.log("=-".repeat(11))
+console.log("Contar itens do array")
+console.log("=-".repeat(11))
+
+const cidades = ["Joinville", "Araquari", "Jaraguá do Sul"]
+
+console.log(`Temos um total de ${cidades.length} cidades na nossa lista!`)
+console.log("=-".repeat(22))
