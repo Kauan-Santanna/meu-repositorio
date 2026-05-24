876e445d3d908b22e6793b16c83f3568b5412b23
Kauan-Santanna <1kn.santanna@gmail.com>
2026-05-24 03:26:31 -0300

    Desafio041 concluido com JavaScript

---
+++ cursos-alura/carreira-front-end/javascript/praticando/desafios/desafio041.js
@@ -0,0 +1,13 @@
+// Crie um array com 5 nomes. Use for para imprimir cada nome no console, um por linha. 
+
+console.log("=-".repeat(7))
+console.log("Lista de nomes")
+console.log("=-".repeat(7))
+
+const nomes = ["Kauan", "Nascimento", "Santanna", "Ana", "Clara"]
+
+for (let i = 0; i < nomes.length; i++) {
+    console.log(nomes[i])
+}
+
+console.log("=-".repeat(7))
