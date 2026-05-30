b7e5fe38df478842d4c9daae98d469bd9e69a522
Kauan-Santanna <1kn.santanna@gmail.com>
2026-05-30 12:34:52 -0300

    Desafio052 concluido com JavaScript

---
+++ cursos-alura/carreira-front-end/javascript/praticando/desafios/desafio052.js
@@ -0,0 +1,14 @@
+// Acesse e exiba o valor da propriedade "nome" no console. 
+
+console.log("=-".repeat(11))
+console.log("Acessando propriedades")
+console.log("=-".repeat(11))
+
+const pessoal = {
+  nome: "Kauan",
+  idade: 21,
+  profissao: "Assistente de P&D",
+};
+
+console.log("Nome:", pessoal.nome);
+console.log("=-".repeat(6));
