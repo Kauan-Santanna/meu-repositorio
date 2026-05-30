47df93d927efa6c6d234f88f76cb6195af3bd462
Kauan-Santanna <1kn.santanna@gmail.com>
2026-05-30 11:28:02 -0300

    Aprendendo sobre objetos

---
+++ cursos-alura/carreira-front-end/javascript/praticando/objetos.js
@@ -0,0 +1,21 @@
+const pessoa = {
+  nome: "Kauan",
+  idade: 21,
+  temCNH: true,
+};
+
+pessoa.sobrenome = "Santanna";
+
+console.log("Nome:", pessoa.nome);
+console.log("Sobrenome:", pessoa.sobrenome);
+
+const livro = {
+  titulo: "O Hobbit",
+  autor: "J. R. R. Tolkien",
+  paginas: 310,
+};
+
+livro.publicado = true
+livro.idiomas = [
+    "Ingles", "Frances", "Portugues", "Espanhol"
+]
