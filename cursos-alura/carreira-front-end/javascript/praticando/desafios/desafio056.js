35bc6ac403915f76219487a11ea29e50e32a77e6
Kauan-Santanna <1kn.santanna@gmail.com>
2026-05-30 14:36:54 -0300

    Desafio056 concluido com JavaScript

---
+++ cursos-alura/carreira-front-end/javascript/praticando/desafios/desafio056.js
@@ -0,0 +1,27 @@
+// Crie um array com 3 objetos, cada um representando uma pessoa com nome e idade.
+
+console.log("=-".repeat(8));
+console.log("Lista de pessoas");
+console.log("=-".repeat(8));
+
+const pessoa1 = {
+  nome: "Vitor",
+  idade: 22,
+  estado: "São Paulo",
+};
+
+const pessoa2 = {
+  nome: "Paulo",
+  idade: 31,
+  estado: "Paraná",
+};
+
+const pessoa3 = {
+  nome: "Robson",
+  idade: 40,
+  estado: "Rio Grande do Sul",
+};
+
+const pessoas = [pessoa1, pessoa2, pessoa3];
+
+console.log(pessoas);
