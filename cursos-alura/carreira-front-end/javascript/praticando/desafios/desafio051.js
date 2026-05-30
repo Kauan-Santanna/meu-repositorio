66051cb58201045e5ef98b5da3f89a7a0992754d
Kauan-Santanna <1kn.santanna@gmail.com>
2026-05-30 12:32:29 -0300

    Desafio051 concluido com JavaScript

---
+++ cursos-alura/carreira-front-end/javascript/praticando/desafios/desafio051.js
@@ -0,0 +1,14 @@
+// Crie um objeto com seu nome, idade e profissão.
+
+console.log("=-".repeat(13));
+console.log("Criando um objeto pessoal");
+console.log("=-".repeat(13));
+
+const pessoal = {
+  nome: "Kauan",
+  idade: 21,
+  profissao: "Assistente de P&D",
+};
+
+console.log(pessoal);
+console.log("=-".repeat(30));
