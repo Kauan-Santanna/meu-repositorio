1545d8cfb7fbccb707c797881599d186f8412b76
Kauan-Santanna <1kn.santanna@gmail.com>
2026-05-31 13:10:27 -0300

    Desafio062 concluido com JavaScript

---
+++ cursos-alura/carreira-front-end/javascript/praticando/desafios/desafio062.js
@@ -0,0 +1,14 @@
+// Crie um array com 3 linguagens de programação. Use destructuring para criar variáveis ling1, ling2 e ling3. 
+
+console.log("=-".repeat(12))
+console.log("Destructuring em arrays")
+console.log("=-".repeat(12))
+
+const linguagens = ["Python", "JavaScript", "Html"]
+
+const [ling1, ling2, ling3] = linguagens
+
+console.log("Linguagem 1:", ling1)
+console.log("Linguagem 2:", ling2)
+console.log("Linguagem 3:", ling3)
+console.log("=-".repeat(12))
