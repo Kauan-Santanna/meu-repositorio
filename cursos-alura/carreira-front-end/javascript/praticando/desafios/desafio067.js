944cbdd572306e464158deca7f70b28eb4173242
Kauan-Santanna <1kn.santanna@gmail.com>
2026-06-01 20:08:08 -0300

    Desafio067 concluido com JavaScript

---
+++ cursos-alura/carreira-front-end/javascript/praticando/desafios/desafio067.js
@@ -0,0 +1,12 @@
+// Crie uma variável com a data atual e exiba o dia, mês e ano formatados.
+
+console.log("=-".repeat(11));
+console.log("Trabalhando com datas");
+console.log("=-".repeat(11));
+
+const data = new Date();
+
+console.log("Dia:", data.getDay());
+console.log("Mês:", data.getMonth());
+console.log("Ano:", data.getFullYear());
+console.log("=-".repeat(11))
