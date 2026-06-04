569dea46332ff17edb65da4cc07b59c5ba379867
Kauan-Santanna <1kn.santanna@gmail.com>
2026-06-04 12:54:12 -0300

    Iniciando o temporaziador e colocando efeitos sonoros

--- cursos-alura/carreira-front-end/javascript/projeto-fokus/script.js
+++ cursos-alura/carreira-front-end/javascript/projeto-fokus/script.js
@@ -5,9 +5,18 @@ const longoBt = document.querySelector(".app__card-button--longo");
 const banner = document.querySelector(".app__image");
 const titulo = document.querySelector(".app__title");
 const botoes = document.querySelectorAll(".app__card-button");
+const startPauseBt = document.querySelector("#start-pause");
+
 const musicaFocoInput = document.querySelector("#alternar-musica");
 const musica = new Audio("/sons/luna-rise-part-one.mp3");
-musica.loop = true
+const audioPlay = new Audio("/sons/play.wav");
+const audioPausa = new Audio("sons/pause.mp3");
+const audioTempoFinalizado = new Audio("/sons/beep.mp3");
+
+let tempoDecorridoEmSegundos = 5;
+let intervaloId = null;
+
+musica.loop = true;
 
 musicaFocoInput.addEventListener("change", () => {
   if (musica.paused) {
@@ -41,19 +50,48 @@ function alterarContexto(contexto) {
   switch (contexto) {
     case "foco":
       titulo.innerHTML = `
-      Otimize sua produtividade,<strong class="app__title-strong">mergulhe no que importa.</strong>
+      Otimize sua produtividade,<br/ >
+      <strong class="app__title-strong">mergulhe no que importa.</strong>
       `;
       break;
     case "descanso-curto":
       titulo.innerHTML = `
-      Que tal dar uma respirada?<strong class="app__title-strong">Faça uma pausa curta!</strong>
+      Que tal dar uma respirada? <strong class="app__title-strong">Faça uma pausa curta!</strong>
       `;
       break;
     case "descanso-longo":
       titulo.innerHTML = `
-      Hora de voltar à superfície.<strong class="app__title-strong">Faça uma pausa longa.</strong>
+      Hora de voltar à superfície. <strong class="app__title-strong">Faça uma pausa longa.</strong>
       `;
     default:
       break;
   }
+}
+
+const contagemRegressiva = () => {
+  if (tempoDecorridoEmSegundos <= 0) {
+    audioTempoFinalizado.play();
+    alert("Tempo finalizado!");
+    zerar();
+    return;
+  }
+  tempoDecorridoEmSegundos -= 1;
+  console.log("Temporizador: " + tempoDecorridoEmSegundos);
+};
+
+startPauseBt.addEventListener("click", iniciarOuPausar);
+
+function iniciarOuPausar() {
+  if (intervaloId) {
+    audioPausa.play();
+    zerar();
+    return;
+  }
+  audioPlay.play();
+  intervaloId = setInterval(contagemRegressiva, 1000);
+}
+
+function zerar() {
+  clearInterval(intervaloId);
+  intervaloId = null;
 }
