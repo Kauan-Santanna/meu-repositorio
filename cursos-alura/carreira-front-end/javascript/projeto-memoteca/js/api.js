1fa2d866c6ca1574de6c663d376f78a0be5f077d
Kauan-Santanna <1kn.santanna@gmail.com>
2026-06-10 21:55:17 -0300

    Alterando dados com o PUT

--- cursos-alura/carreira-front-end/javascript/projeto-memoteca/js/api.js
+++ cursos-alura/carreira-front-end/javascript/projeto-memoteca/js/api.js
@@ -3,7 +3,7 @@ const api = {
     try {
       const response = await fetch("http://localhost:3000/pensamentos");
       return await response.json();
-    } catch (error) {
+    } catch {
       alert("Erro ao buscar pensamentos");
       throw error;
     }
@@ -20,10 +20,37 @@ const api = {
       });
       return await response.json();
     } catch (error) {
-      alert("Erro ao buscar pensamentos");
+      alert("Erro ao salvar pensamento");
       throw error;
     }
   },
-};
+
+  async buscarPensamentoPorId(id) {
+    try {
+      const response = await fetch(`http://localhost:3000/pensamentos/${id}`);
+      return await response.json();
+    } catch {
+     alert("Erro ao buscar pensamento");
+      throw error;
+    }
+  },
+
+  async editarPensamento(pensamento) {
+    try {
+      const response = await fetch(`http://localhost:3000/pensamentos/${pensamento.id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(pensamento),
+      });
+      return await response.json();
+    } catch (error) {
+      alert("Erro ao editar pensamento");
+      throw error;
+    }
+  },
+
+}
 
 export default api;
