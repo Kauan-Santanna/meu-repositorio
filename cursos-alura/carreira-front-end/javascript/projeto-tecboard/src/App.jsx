13b6c1a8a84eedc8e61802e225772be2e3713178
Kauan-Santanna <1kn.santanna@gmail.com>
2026-06-16 19:51:29 -0300

    Criando o primeiro componente

--- cursos-alura/carreira-front-end/javascript/projeto-tecboard/src/App.jsx
+++ cursos-alura/carreira-front-end/javascript/projeto-tecboard/src/App.jsx
@@ -1,4 +1,18 @@
-import './App.css'
+import "./App.css";
+
+// no react, componentes são FUNÇÕES
+
+function FormularioDeEvento() {
+  return (
+    <form className="form-evento">
+      <h2>Preencha para criar um evento:</h2>
+      <fieldset>
+        <label htmlFor="nome">Qual o nome do evento?</label>
+        <input type="text" id="nome" />
+      </fieldset>
+    </form>
+  );
+}
 
 function App() {
   return (
@@ -9,8 +23,9 @@ function App() {
       <section>
         <img src="/banner.png" alt="" />
       </section>
+      <FormularioDeEvento />
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
