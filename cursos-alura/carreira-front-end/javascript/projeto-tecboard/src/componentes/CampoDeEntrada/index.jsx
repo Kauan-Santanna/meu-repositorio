28e912e2c812eb2a2b79b6d9d54378c74eb8f425
Kauan-Santanna <1kn.santanna@gmail.com>
2026-06-17 20:23:27 -0300

    Estilizando componentes

--- cursos-alura/carreira-front-end/javascript/projeto-tecboard/src/componentes/CampoDeEntrada/index.jsx
+++ cursos-alura/carreira-front-end/javascript/projeto-tecboard/src/componentes/CampoDeEntrada/index.jsx
@@ -1,3 +1,5 @@
+import "./campo-entrada.css";
+
 export function CampoDeEntrada(props) {
-  return <input {...props} />;
+  return <input {...props} className="campo-entrada-form"/>;
 }
--- cursos-alura/carreira-front-end/javascript/projeto-tecboard/src/componentes/FormularioDeEvento/index.jsx
+++ cursos-alura/carreira-front-end/javascript/projeto-tecboard/src/componentes/FormularioDeEvento/index.jsx
@@ -1,4 +1,4 @@
-import "./formulario-de-evento.css"
+import "./formulario-de-evento.css";
 import { CampoDeEntrada } from "../CampoDeEntrada";
 import { CampoDeFormulario } from "../CampoDeFormulario";
 import { Label } from "../Label";
@@ -8,10 +8,16 @@ export function FormularioDeEvento() {
   return (
     <form className="form-evento">
       <TituloFormulario>Preencha para criar um evento:</TituloFormulario>
-      <CampoDeFormulario>
-        <Label htmlFor="nome">Qual o nome do evento?</Label>
-        <CampoDeEntrada type="text" id="nome" placeholder="Summer dev hits" />
-      </CampoDeFormulario>
+      <div className="campos">
+        <CampoDeFormulario>
+          <Label htmlFor="nome">Qual o nome do evento?</Label>
+          <CampoDeEntrada type="text" id="nomeEvento" placeholder="Summer dev hits" name="nomeEvento" />
+        </CampoDeFormulario>
+        <CampoDeFormulario>
+          <Label htmlFor="dataEvento">Data do evento</Label>
+          <CampoDeEntrada type="date" id="dataEvento" nome="dataEvento" />
+        </CampoDeFormulario>
+      </div>
     </form>
   );
 }
